5c0f4da19cab59aeaa9b15206614cbdab45b4a2d
fiekzz <fikrichuck@gmail.com>
2023-01-21 17:29:07 +0800

    Main.java documentation🧾

--- Main.java
+++ Main.java
@@ -2,16 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Array of question from the template
         String[] strArr = {"1+2", "a+b", "1+2*6", "(1+2)*6", "(1+2*6"};
         
+        // declaring converter variable
+        PostfixConverter converter = new PostfixConverter();
+
+        // iterate each question
         for(int i = 0; i < strArr.length; i++) {
 
-            PostfixConverter converter = new PostfixConverter();
-
+            // convert the question into postfix form
             converter.calculate(strArr[i]);
 
+            // calculate the postfix conversion
             CalculatePostfix calcPostfix = new CalculatePostfix(converter.getStringPostFix());
 
+            // display the result
             System.out.println(calcPostfix.getResult());
 
         }
